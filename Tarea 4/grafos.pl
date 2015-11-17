63b36ace0494104ebf957ca994ca5f0e49a220af
elGranCali <cali2910@gmail.com>
2015-11-16 23:33:22 -0600

    Mas avance
    Falta evitar repetidos de arcos

--- Tarea 4/grafos.pl
+++ Tarea 4/grafos.pl
@@ -3,20 +3,17 @@ posicion(X, [_|R], P) :- posicion(X,R,M), P is M + 1.
 %Metodo que resuelve las adyacencias
 adj(_,[],[]).	
 adj(X,[A|B],P) :- adj(X,B,C) , append([[X,A]], C, P),!.
-% adj(X,[A|B], R) :-  
-test(_,[A|B],P) :- append([x,y,s], [A|B], P).
-test2([A|B],A).
-listascomponentes([], []).
-listascomponentes([H|T], X) :- H, listascomponentes(T,_).
-%listascomponentes([H|T], X) :- append(H, listascomponentes(T,_), X).
+
+% Definicion de los operandos
 :- op(500, xfy, -).
 :- op(500, xfy, >).
 
 get-nodos([],[]).
-get-nodos([H|T],D) :- findall(X, H\=X-Y, B), get-nodos(T,C), append(B,C,D).  
+get-nodos([H|T],D) :- get-nodos(T,B), (atomic(H), M=[H] ; M=[]), append(B,M,D). 
 
+% Ejemplo componentes-arcos([a-a, h-a, t, b-a, u, a-a, t], X).
 componentes-arcos([], []).
-componentes-arcos(A, U) :- findall(X, not(member(X-_, A)),B1), findall(X, member(X-_, A),B2), append(B1,B2,B), findall(Q, member(_-Q, A),C), append(B,C,D), sort(D,E), findall(arco(N,V),member(N-V, A),F), append([E], F, U).
+componentes-arcos(A, U) :- get-nodos(A,B1), findall(X, member(X-_, A),B2), append(B1,B2,B), findall(Q, member(_-Q, A),C), append(B,C,D), sort(D,E), setof(arco(N,V),member(N-V, A),F), append([E], F, U).
 
 dcomponentes-arcos([], []).
 dcomponentes-arcos(A, U) :- findall(X, member(X>_, A),B), findall(Q, member(_>Q, A),C), append(B,C,D), sort(D,E), findall(arco(N,V),member(N>V, A),F), append([E], F, U).
