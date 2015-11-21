a4694a80ce9614cfbaaad518cf4b9245e049f66e
alesk8400 <alesk8400@gmail.com>
2015-11-20 21:22:49 -0600

    Funca componentes a listas

--- Tarea 4/TP-A76440.pl
+++ Tarea 4/TP-A76440.pl
@@ -13,8 +13,8 @@
 % No imprime la respuesta completa, pero esta sí está guardada en la base de datos de prolog.
 %listas-componentes(L,R).
 
-listas-componentes([],[]).
-listas-componentes([A|B],[S]) :-  guardarTodasAdj([A|B]) , setof(X,Y^adj(X,Y),R), armarArcos(R), setof(arco(W,Z),arco(W,Z),M), armarRepresentacion(R,M), setof(grafo(U,V),grafo(U,V),S).
+listasComponentes([],[]).
+listasComponentes([A|B],[S]) :-  guardarTodasAdj([A|B]) , setof(X,Y^adj(X,Y),R), armarArcos(R), setof(arco(W,Z),arco(W,Z),M), armarRepresentacion(R,M), setof(grafo(U,V),grafo(U,V),S).
 
 guardarTodasAdj([]).
 guardarTodasAdj([X|Y]) :- assert(X) , guardarTodasAdj(Y).
@@ -29,6 +29,50 @@ armarArcosAux(H,[[J|L]|K]) :- assert(arco(H,J)) , armarArcosAux(H,[L]),!.
 armarRepresentacion([],[]).
 armarRepresentacion(R,M) :- assert(grafo(R,M)).
 
+% --- componentesListas/2(+L,-R) ----------------------------------------------------------
+%     R es la representación en listas de adyacencia del grafo no dirigido
+%
+% --- ejemplos: 
 
+componentesListas([],[]).
+componentesListas(A,[S]) :- assert(arco(_,[])), guardarGrafo(A), setof(X,Y^grafo(X,Y),R), setof(N,M^grafo(M,N),O), guardarArcos(O), armarListas(R).
 
+armarListas([]).
+armarListas([[J|L]|K]) :- setof(W,J^arco(J,W),G), armarListasAux(J,G), armarListas(L),!.
+armarListas([J|L]) :- setof(W,J^arco(J,W),G), armarListasAux(J,G), armarListas(L),!.
 
+armarListasAux([],[]).
+armarListasAux(T,U) :- assert(adj(T,U)),!.
+
+guardarGrafo([]).
+guardarGrafo(A) :- assert(A).
+
+guardarArcos([]).
+guardarArcos([[X|Y]|K]) :- assert(X), guardarArcos(Y),!.
+guardarArcos([X|Y]) :- assert(X), guardarArcos(Y),!.
+
+% --- diComponentesListas/2(+L,-R) ----------------------------------------------------------
+%     R es la representación en listas de adyacencia del grafo dirigido
+%
+% --- ejemplos: diComponentesListas(grafo([a,b,c,d,e,g,h],[arco(a,b), arco(a,d), arco(b,d), arco(d,g), arco(e,h)]),T).
+%T = [[adj(a, [b, d, []]), adj(b, [d, []]), adj(c, [[]]), adj(d, [g, []]), adj(e, [h, []]), adj(g, [[]]), adj(h, [...])]].
+%Nota: para que el programa trabajara de manera correcta los nodos que no son adyacentes a ningún otro se incluyó
+%el assert(arco(_,[])), lo cual causa que quede un item vacío añadido a cada lista de adyacencia, pero aún así,
+%la respuesta del programa es correcta. Además no imprime la respuesta completa, pero esta sí está guardada en la base de datos de prolog.
+
+diComponentesListas([],[]).
+diComponentesListas(A,[S]) :- assert(arco(_,[])), diGuardarGrafo(A), setof(X,Y^grafo(X,Y),R), setof(N,M^grafo(M,N),O), diGuardarArcos(O), diArmarListas(R), setof(adj(U,V),adj(U,V),S).
+
+diArmarListas([]).
+diArmarListas([[J|L]|K]) :- setof(W,J^arco(J,W),G), diArmarListasAux(J,G), diArmarListas(L),!.
+diArmarListas([J|L]) :- setof(W,J^arco(J,W),G), diArmarListasAux(J,G), diArmarListas(L),!.
+
+diArmarListasAux([],[]).
+diArmarListasAux(T,U) :- assert(adj(T,U)),!.
+
+diGuardarGrafo([]).
+diGuardarGrafo(A) :- assert(A).
+
+diGuardarArcos([]).
+diGuardarArcos([[X|Y]|K]) :- assert(X), diGuardarArcos(Y),!.
+diGuardarArcos([X|Y]) :- assert(X), diGuardarArcos(Y),!.
